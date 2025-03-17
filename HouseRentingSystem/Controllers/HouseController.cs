bbee8d2f063401ac3442dbc244a9d51e7d156df0
demir <demirdemirev101@gmail.com>
2025-03-17 15:10:44 +0200

    Altered authorization.

--- HouseRentingSystem/Controllers/HouseController.cs
+++ HouseRentingSystem/Controllers/HouseController.cs
@@ -4,13 +4,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HouseRentingSystem.Controllers
 {
+    [Authorize]
     public class HouseController : Controller
     {
+        [AllowAnonymous]
         public async Task<IActionResult> All()
         {
             return View(new AllHousesQueryModel());
         }
-        [Authorize]
         public async Task<IActionResult> Mine()
         {
             return View(new AllHousesQueryModel());
@@ -19,46 +20,39 @@ namespace HouseRentingSystem.Controllers
         {
             return View(new HouseDetailsViewModel());
         }
-        [Authorize]
+       
         public async Task<IActionResult> Add()
         {
             return View();
         }
-        [Authorize]
         [HttpPost]
         public async Task<IActionResult> Add(HouseFormModel model)
         {
             return RedirectToAction(nameof(Details), new { id = "1" });
         }
-        [Authorize]
         public async Task<IActionResult> Edit(int id)
         {
             return View(new HouseFormModel());
         }
-        [Authorize]
         [HttpPost]
         public async Task<IActionResult> Edit(int id, HouseFormModel house)
         {
             return RedirectToAction(nameof(Details), new { id = "1" });
         }
-        [Authorize]
         public async Task<IActionResult> Delete(int id)
         {
             return View(new HouseFormModel());
         }
-        [Authorize]
         [HttpPost]
         public async Task<IActionResult> Delete(HouseDetailsViewModel house)
         {
             return RedirectToAction(nameof(All));
         }
-        [Authorize]
         [HttpPost]
         public async Task<IActionResult> Rent(int id)
         {
             return RedirectToAction(nameof(Mine));
         }
-        [Authorize]
         [HttpPost]
         public async Task<IActionResult> Leave(int id)
         {
